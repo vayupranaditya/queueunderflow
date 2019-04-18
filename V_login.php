71921aac164381cd7b8a6a0f2b122e5c89b4853b
Alifian Diasanda <alifiandiasanda@gmail.com>
2019-04-18 19:42:58 +0700

    Add files via upload

---
+++ V_login.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Log in</title>
+	<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+	<style>
+        body {
+        background: #eee;
+        font-family: calibri;
+        }
+        .main {
+          width: 350px;
+          background: white;
+          padding: 15px;
+          margin: 250px auto;
+          box-shadow: 0px 0px 8px #1234;
+          border-radius: 5px;
+        }
+        button, table, input {
+          width: 100%;
+        }
+        .btn-primary, .btn-primary:hover, .btn-primary:active {
+          background: #0099ff !important;
+          cursor: pointer;
+        }
+        .title {
+          font-size: 26px;
+          margin-bottom: 10px;
+        }
+        label {
+          font-weight: normal;
+        }
+  </style>
+</head>
+<body>
+	<form class="main">
+		<center>
+			<div class="title">Log in</div>
+		</center>
+	    <div class="form-group">
+	      	<label>Email</label>
+	      	<input type="text" class="form-control" id="email">
+          
+	    </div>
+	    <div class="form-group">
+	      	<label>Password</label>
+	      	<input type="text" class="form-control" id="password">
+          
+	    </div>
+	    <button onclick="hitungPythagoras_1301174330()" class="btn btn-primary">Log in</button>
+	</form>
+</body>
+</html>
